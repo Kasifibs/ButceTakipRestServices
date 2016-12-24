e3664e45ac7f0b4618076bd1fad11afa91140055
Kasifibs <dispinar01@gmail.com>
2016-12-25 01:23:16 +0300

    Logout işlemi

--- src/main/java/com/dispinar/butcetakip/server/controller/LoginController.java
+++ src/main/java/com/dispinar/butcetakip/server/controller/LoginController.java
@@ -1,7 +1,13 @@
 package com.dispinar.butcetakip.server.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +24,14 @@ public class LoginController {
 	@RequestMapping(value="/kontrol", method=RequestMethod.GET)
 	public ResponseEntity<Boolean> checkStatus(){
 		return new ResponseEntity<Boolean>(Boolean.TRUE, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/oturumKapat", method = RequestMethod.GET)
+	public ResponseEntity<Boolean> logoutPage (HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return new ResponseEntity<Boolean>(Boolean.TRUE, HttpStatus.OK);
 	}
 }
